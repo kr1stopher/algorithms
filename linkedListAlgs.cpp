aa61947cf2d661237702d0329fdfd47ad7432348
Kris <swartzbaugh.k@gmail.com>
2021-09-12 11:07:53 -0700

    added kth  to last alg linked list

--- linkedListAlgs.cpp
+++ linkedListAlgs.cpp
@@ -55,7 +55,6 @@ void removeDuplictes(node n){
   while (m1!=nullptr){
     bool duplicate = false;
     for (int i =0; i<stored.size(); i++){
-      cout<<"(m1->data, stored[i])"<<m1->data<<" , "<<stored[i]<<endl;
       if (stored[i]==m1->data){
         duplicate = true; //delete node
         m->next = m1->next;
@@ -64,7 +63,6 @@ void removeDuplictes(node n){
         break;
       }
     }
-    cout<<stored[stored.size()-1]<<endl;
     if (!duplicate){
       //stored.push_back(m->data);
       m = m->next;
@@ -74,19 +72,49 @@ void removeDuplictes(node n){
   }
 }
 
-void  
+//alg to find the kth to last element of a singly linked list
+int  kthElement (int k, node n) {
+  if (k<1) //return error if k is too small
+  {
+    cerr<<endl<<"Invalid  input k<1"<<endl;
+    exit(1);
+  }
+  vector<int> elements;
+  node* m = & n;
+
+  //go through the linked list adding the data of each node to elements
+  while (m!=nullptr){
+    elements.push_back(m->data);
+    m = m->next;
+  }
+
+  //check to make sure that k is not too large for the linked list
+  if (k>elements.size())
+  {
+    cerr<<endl<<"Invalid input k>length of list"<<endl;
+    exit(1);
+  }
+
+  //return the kth to last element
+  return elements[elements.size()-k];
+}
 
 int main(){
 
+  //create a linked list using the node class for testing algs
   node node1 = node(1);
   int a[] = {2,3,3,4,5,6,7,3};
   for (int i = 0;i< sizeof(a)/sizeof(a[0]); i++){
     node1.appendNode(a[i]);
   }
+
+
+  //testing algs
   node1.printList();
-//removeDuplictes(node1);
   removeDuplictes(node1);
   cout<<"Below is the list with duplicates removed"<<endl;
   node1.printList();
+  cout<<"2nd ot last element is "<<kthElement(2,node1)<<endl;
+
 
 }
