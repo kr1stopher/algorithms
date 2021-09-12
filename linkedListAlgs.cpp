fed330a72b352d937a98c8da4403c181ed4e91b5
Kris <swartzbaugh.k@gmail.com>
2021-09-11 19:09:15 -0700

    updated linkedListAlgs

--- linkedListAlgs.cpp
+++ linkedListAlgs.cpp
@@ -4,43 +4,11 @@
 #include <cstdio>
 #include <stdio.h>
 #include <cstddef>
+#include <vector>
 
 
 using namespace std;
 
-/*
-class node1 {
-public:
-    int data;
-    node* next = nullptr;
-
-    node (int info){
-      this->data = info;
-      this-> next = nullptr;
-    }
-
-    void appendNode (int info){
-      node* end = new node(info);
-      node* n = this;
-      while (n->next != nullptr){
-        n =n->next;
-      }
-      n->next = end;
-      end->next = nullptr;
-    }
-
-
-    void printList(){
-      int i = 0;
-      node* n = this;
-      while (n->next !=nullptr){
-        cout<<n->data<<endl;
-        n = n->next;
-      }
-      cout<<n->data<<endl;
-      }
-};
-*/
 
 class node {
 public:
@@ -51,12 +19,17 @@ public:
     this->data = info;
   }
 
-  void appendNode (data){
-
+  void appendNode (int data){
+    node* end = new node(data);
+    node* n = this;
+    while (n->next != nullptr){
+      n = n->next;
+    }
+    n->next = end;
   }
 
   void printList () {
-    i = 0;
+    int i = 0;
     node* n = this;
     while (n != nullptr){
       cout<<n->data<<"This is node: "<<i<<endl;
@@ -65,20 +38,87 @@ public:
     }
   }
 
+  //friend void removeDuplictes(node n);
+  void removeDuplictes(){
+    node* m = this; //m will be the first pointer
+
+    if (m->next == nullptr){
+      return;
+    }
+
+    node* m1 = m->next; //m1 will be the further ahead pointer
+    vector<int> stored;
+    stored.push_back(m->data);
+    while (m1->next!=nullptr){
+      bool duplicate = false;
+      for (int i =0; i<stored.size()-1; i++){
+        if (stored[i]==m->data){
+          duplicate = true; //delete node
+          m->next = m1->next;
+          m1 = m->next; //need to add case to check if m is null ptr
+          cout<<"at least trying to delete"<<endl;
+        }
+      }
+
+      stored.push_back(m->data);
+      cout<<stored[stored.size()-1]<<endl;
+      if (duplicate == false){
+        m = m->next;
+        m1 = m1->next;
+      }
+    }
+
+
+  }
+};
+
+/*
+//Write code to remove duplicates from an unsorted linked list.
+void removeDuplictes(node n){
+  node* m = & n; //m will be the first pointer
+  if (m->next = nullptr)
+    return;
+  node* m1 = m->next; //m1 will be the further ahead pointer
+  vector<int> stored;
+  m1 = m1->next;
+  cout<<"just before zz"<<endl;
+  node* z = m1->next;
+  cout<<"in here"<<endl;
+  while (m1->next!=nullptr){
+    bool duplicate = false;
+    cout<<"in here 2.0 bitches"<<endl;
+    for (int i =0; i<stored.size()-1; i++){
+      if (stored[i]==m1->data){
+        duplicate = true; //delete node
+        m->next = m1->next;
+        m1 = m->next; //need to add case to check if m is null ptr
+      }
+    }
+
+    stored.push_back(m->data);
+    if (duplicate == false){
+      m = m->next;
+      m1 = m1->next;
+    }
+  }
 
 
 }
+*/
 
 
 
 int main(){
-  
-  node node = node(1);
-  /*
-  int a[] = {2,3,4,5,6,7,8};
+
+  node node1 = node(1);
+  int a[] = {2,3,3,4,5,6,7,8};
   for (int i = 0;i< sizeof(a)/sizeof(a[0]); i++){
     node1.appendNode(a[i]);
   }
   node1.printList();
-  */
+//removeDuplictes(node1);
+  node1.removeDuplictes();
+  cout<<"Below is the list with duplicates removed"<<endl;
+  node1.printList();
+
 }
