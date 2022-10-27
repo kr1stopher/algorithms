7b7dc255bdd14692ab3c560bd604ee3bb4b7a801
kr1stopher <76436762+kr1stopher@users.noreply.github.com>
2022-10-27 10:07:42 -0700

    Add files via upload

--- algs1.cpp
+++ algs1.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
 #include <memory>
+#include <string>
+#include <map>
+#include <vector>
+#include<algorithm>
 
 
 
@@ -118,8 +122,166 @@ void user_int (){
 
 }
 
+void shift_string(){
+    string myString;
+    cout<<"Please enter the string you wish to shift"<<endl;
+    cin>>myString;
+
+    int i = 0; 
+    while (i<myString.length()){
+        myString[i]++;
+        i++;
+    }
+    cout<<myString<<endl;
+}
+
+
+int find_odd(int input[], int size){
+    map<int, int> totals;
+    map<int, int>::iterator it;
+    int i = 0; 
+    while (i<size){
+        it = totals.find(input[i]);
+        if (it == totals.end()){
+            totals.insert(make_pair(input[i], 1));
+        }else {
+            it->second++;
+        }
+        i++;
+    }
+
+    for (auto const& i: totals){
+        if (i.second % 2 != 0){
+            cout<<i.first<<" has odd entries"<<endl;
+        }
+
+    }
+
+    return -1;
+}
+
+void test_vector(){
+    vector<int> vec;
+    for (int i =0;i<8;i++){
+        vec.push_back(i);
+    }
+    for (auto const& j : vec){
+        cout<<j<<endl;
+    }
+
+}
+
+void make_box(int n){
+    cout<<endl;
+    string top(n, '#');
+    string center(n-2, ' ');
+    center = '#' + center + '#';
+
+    for (int i = 0; i<n; i++){
+        if (i==0 or i==n-1){
+            cout<<top<<endl;
+        }else {
+        cout<<center<<endl;
+        }
+    }
+
+}
+
+int collatz(int n){
+    cout<<endl<<n<<endl;
+    while (n > 1){
+        if (n%2 == 0){
+            n=n/2;
+        }else {
+            n=n*3+1;
+        }
+        cout<<n<<endl;
+    }
+    return n;
+}
+
+int prison(int prison[], int size){
+    if (prison[0] == 0) {return 0;};
+    int unlocked = 1;
+    int freed = 0;
+    for (int i =0; i<size; i++){
+        if (prison[i] == unlocked){
+            freed++;
+            unlocked = abs(unlocked - 1);
+        }
+    }
+    cout<<endl<<freed<<" is the total freed"<<endl;
+    return freed;
+}
+
+int freedPrisoners(vector<int> prison) {
+    if (prison[0] == 0) {return 0;};
+    int unlocked = 1;
+    int freed = 0;
+    for (auto const& i:prison){
+        if (i == unlocked){
+            freed++;
+            unlocked = abs(unlocked - 1);
+        }
+    }
+    return freed;
+}
+
+bool same_letter(string one, string two){
+    bool isSame=true;
+    map<char, vector<int>> pattern1;
+    map<char, vector<int>> pattern2;
+    vector<char> seen1;
+    vector<char> seen2;
+    int i = 0;
+    cout<<"made it"<<endl;
+    for (int i =0; i<one.length(); i++){
+        if (find(seen1.begin(), seen1.end(), one[i]) ==seen1.end()){
+            seen1.push_back(one[i]);
+            vector<int> initialize = {i};
+            pattern1.insert(make_pair(one[i], initialize));
+            cout<<one[i]<<"added"<<endl;
+        }else {
+            pattern1.find(one[i])->second.push_back(i);
+        }
+    }
+    cout<<pattern1.find(one[i])->second[0];
+    cout<<"checkpoint"<<endl;
+    for (auto const&i:pattern1.find(one[i])->second){
+        cout<<i<<endl;
+    }
+    for (int i =0; i<two.length(); i++){
+        if (find(seen2.begin(), seen2.end(), two[i]) ==seen2.end()){
+            seen2.push_back(two[i]);
+            vector<int> initialize = {i};
+            pattern2.insert(make_pair(two[i], initialize));
+            cout<<two[i]<<"added"<<endl;
+        }else {
+            pattern2.find(two[i])->second.push_back(i);
+        }
+    }
+
+    //check if they are the same:
+    for (int i =0; i<seen1.size();i++){
+        if (pattern1.find(seen1[i])->second !=  pattern2.find(seen2[i])->second){
+            isSame = false;
+        }
+    }
+
+    return isSame;
+}
+
+class shape{
+    protected:
+        int width; height
+
+};
+
+
 
 int main() {
+
+    /*
     //pass using move function 
     unique_ptr<int> myptr = make_unique<int>();
     *myptr = 9;
@@ -129,7 +291,7 @@ int main() {
     myFunc2(myptr2);
     cout<<endl<<*myptr2<<" in main"<<endl;
 
-
+>>
     unique_ptr<rectangle> rect = make_unique<rectangle>();
     unique_ptr<triangle> tri = make_unique<triangle>();
     triangle mytri(1,2); 
@@ -146,8 +308,28 @@ int main() {
     newRect->area();
 
     user_int();
+    */
+
+   //shift_string();
+   int test[11] = {0,0,1,1,2,2,3,4,4,5,5};
+   cout<<(sizeof(test)/sizeof(test[0]))<<endl;
+   find_odd(test, (sizeof(test)/sizeof(test[0])));
+   test_vector();
+   make_box(8);
+
+    collatz(5);
+    int myPrison[7] = {1, 1, 0, 0, 0, 1, 0};
+    prison(myPrison, 7);
+
+    vector<int> yPris = {1, 1, 0, 0, 0, 1, 0};
+    cout<<freedPrisoners(yPris)<<endl;
+
+    int * ptr;
+    int i;
+    ptr &i;
 
 
+    cout<<same_letter("aaaacccadd", "bbbbaaabee")<<endl;
     return 0;
 
 }
